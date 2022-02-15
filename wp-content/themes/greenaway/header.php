81460b42853cf1041d03580ff5c9fd4581c335ea
jon <jon.wlive@yahoo.co.uk>
2022-02-15 16:03:36 +0000

    fonts and ACF fields

--- wp-content/themes/greenaway/header.php
+++ wp-content/themes/greenaway/header.php
@@ -19,6 +19,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+	<link rel="stylesheet" href="https://use.typekit.net/kzt7gff.css">
 	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/icons/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/icons/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/icons/favicon-16x16.png">
